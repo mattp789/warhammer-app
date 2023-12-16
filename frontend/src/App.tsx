cca6a478d9d9c879b331b242f5629f36d7a1570e
Ben Hunter <code@benhunter.me>
2023-12-16 14:56:11 -0600

    Cards

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,29 @@
 import {useState} from 'react'
 import warhammerLogo from '/warhammer.png'
 import './App.css'
-import {Button} from "flowbite-react";
+import {Button, Card} from "flowbite-react";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="flex flex-col items-center ">
-      <div >
+    <div className="flex flex-col items-center">
+      <div>
         <a href="https://www.warhammer-community.com/" target="_blank">
           <img src={warhammerLogo} className="logo" alt="Warhammer logo"/>
         </a>
       </div>
-      <h1 className="m-10">Warhammer things</h1>
-      <div className="card m-10">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-      </div>
+      <Card className="m-4">
+        <h1 className="m-4">Warhammer things</h1>
+        <div className="flex m-4 place-content-center">
+          <Button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </Button>
+        </div>
+      </Card>
+      <Card className="m-4">
+        <h1 className="m-4">Roll the Dice!</h1>
+      </Card>
     </div>
   )
 }
