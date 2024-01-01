2755360d26fcf54d74c6d003ad369a69157c24bd
Matthew Parker <matthew.w.parker19.civ@army.mil>
2024-01-01 10:02:24 -0600

    updating

--- src/main/java/com/warhammer/app/ArmyController.java
+++ src/main/java/com/warhammer/app/ArmyController.java
@@ -3,15 +3,15 @@ package com.warhammer.app;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-
 @RestController
-public class AlliancesController {
-    @GetMapping("/getalliances")
-    @CrossOrigin(origins = "http://localhost:3000")
-    public ArrayList<Object> Unit() throws JsonProcessingException {
-        return new GetAlliances().getname();
+public class ArmyController {
+    @GetMapping("/getarmy/{name}")
+    @CrossOrigin(origins = "http://localhost:5173")
+    public ArrayList<Object> Army(@PathVariable String name) throws JsonProcessingException {
+        return new Army(name).getarmies();
     }
 }
--- src/main/java/com/warhammer/app/UnitsController.java
+++ src/main/java/com/warhammer/app/UnitsController.java
@@ -1,2 +1,17 @@
-package com.warhammer.app;public class UnitsController {
+package com.warhammer.app;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+@RestController
+public class UnitsController {
+    @GetMapping("/getunits/{army}")
+    @CrossOrigin(origins = "http://localhost:5173")
+    public ArrayList<Object> Units(@PathVariable String army) throws JsonProcessingException {
+        return new Units(army).getunits();
+    }
 }
